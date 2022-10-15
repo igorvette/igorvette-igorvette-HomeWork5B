4a5efaabe1a8b8fea1ff0e99947df15ea3ec5109
Igor <igorvette@mail.ru>
2022-10-15 09:02:44 +0100

    Задача1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход число два числа (А и В) и вохводит числа в натуральную
+// степень (В).
+
+Console.WriteLine("Введите число А");
+int A = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine("Введите число В");
+int B = Convert.ToInt32(Console.ReadLine( ));
+
+if (B >= 0)
+{
+    int result = 1;
+    for (int y = 0; y < B; y ++)
+    {
+         result = result * A;
+    }
+    Console.WriteLine($"{A} в степени {B} присваиваем результат {result}");
+}
+else 
+{
+    Console.WriteLine($"{B} не натуральное число.");
+}
+ 
